53360f9803f3a932a75b8340e39f798586822793
afradenburg <andres.burg@hotmail.com>
2023-08-23 19:11:47 -0500

    'homework zoo'

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,25 +1,21 @@
-import Card from './Card';
- 
+import Card from "./Card";
 
 export default function Cards(props) {
-   const {characters} = props
-   return <div>
-      {
-         characters.map(info => (
-            <Card
-            key = {info.id}
-            name= {info.name} 
-            status = {info.status}
-            species = {info.species} 
-            gender = {info.gender}
-            origin = {info.origin.name} 
-            image = {info.image}
-            onClose= {() => window.alert('Emulamos que se cierra la card')}
-            />
-         ))
-
-        
-      }
-   </div>
-
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((info) => (
+        <Card
+          key={info.id}
+          name={info.name}
+          status={info.status}
+          species={info.species}
+          gender={info.gender}
+          origin={info.origin.name}
+          image={info.image}
+          onClose={() => window.alert("Emulamos que se cierra la card")}
+        />
+      ))}
+    </div>
+  );
 }
